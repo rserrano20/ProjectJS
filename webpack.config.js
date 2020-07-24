9a16dd34685567cfe2d209719b9c18932ac7e2cf
Rosario <roserranoar@gmail.com>
2020-07-23 22:14:50 -0300

    se agrego estructura basica del projecto

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
  
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    index: './src/js/index.js',
+    admin: './src/js/admin.js',
+  },
   output: {
-    filename: 'app.js',
+    filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -15,10 +18,21 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      }
+      },  {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                name: 'img/[name].[ext]',
+             },
+          },
+        ],
+      }       
+
     ]
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
         minify:{
@@ -28,14 +42,36 @@ module.exports = {
           removeScriptTypeAttributes: true,
           removeStyleLinkTypeAttributes: true,
           useShortDoctype: true
-        }
+        },
+        inject:true,
+        chunks:['index']
     }),  
+    new HtmlWebpackPlugin({
+      template: './src/admin.html',
+      minify:{
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      inject:true,
+      chunks:['admin'],
+      filename: './admin.html'
+  }),  
     new MiniCssExtractPlugin({
       filename: 'css/style.css'
-  })
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: './src/img',
+        to: 'img',
+      },
+    ]
+})
 
 ],
-
-
  
 };
